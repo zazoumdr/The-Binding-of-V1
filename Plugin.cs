3d3683d0a96ed1f05f38437d6c7a3391e3624943
Bertrand Killian <killianzazou13@gmail.com>
2026-04-17 17:05:53 +0200

    adding the bepinex template in the project

---
+++ Plugin.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using UnityEngine;
+using HarmonyLib;
+using GameConsole.pcon;
+
+namespace ULTRAKILLBepInExTemplate
+{
+    /// <summary>
+    /// Don't panic if you see errors here! Just follow the instructions in "README.md"
+    /// </summary>
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        /// <summary>
+        /// Make sure the modGUID is unique and is not used by any other mods!
+        /// </summary>
+        private const string modGUID = "modAuthor.modName";
+
+        /// <summary>
+        /// Make sure this is the same modName as the modGUID (without modAuthor)
+        /// </summary>
+        private const string modName = "modName";
+
+        /// <summary>
+        /// Self explainitory.
+        /// </summary>
+        private const string modVersion = "1.0.0";
+
+        private static readonly Harmony Harmony = new Harmony(modGUID);
+
+        /// <summary>
+        /// Anything that goes in here will be executed the exact moment the mod is loaded.
+        /// It is recommended that you log that the mod is loaded when this happens.
+        /// </summary>
+        private void Awake()
+        {
+            Debug.Log($"Mod {modName} version {modVersion} is loading...");
+            Harmony.PatchAll();
+            Debug.Log($"Mod {modName} version {modVersion} is loaded!");
+        }
+
+        /// <summary>
+        /// Every tick this event will trigger (basically this will trigger every single frame)
+        /// </summary>
+        private void Update()
+        {
+            
+        }
+
+        /// Make sure to rename ExamplePatch to be the same class you're patching and make sure to rename Function to a function in that class!
+        /// Example of a HarmonyPatch. (Feel free to delete!)
+        // [HarmonyPatch(typeof(ExamplePatch), "Function")]
+        // public static class ExamplePatch
+        // {
+        ///     // Either use Postfix or Prefix.
+        //     [HarmonyPostfix]
+        //     public static void Postfix()
+        //     {
+        //         // Code here.
+        //     }
+        // }
+    }
+}
---
+++ README.md
@@ -0,0 +1,39 @@
+﻿# ULTRAKILL MODDING TEMPLATE
+Created by "thebluenebula" and made possible with the help of BepInEx.
+# How to Setup The Template
+- First of all, install the template and move it to "[LocalUserFolder]\Documents
+- Navigate to the folder of your version of Visual Studio you would to use the template in. (Visual Studio 20XX)
+- Find the "My Exported Templates" folder and open it.
+- Paste the previously obtained .zip there.
+- When opening Visual Studio, you will find the template as a project template.
+
+# How to Setup References
+**MANDATORY**
+- Install ULTRAKILL with BepInEx.
+- Right click the "lib" folder and then click "Add Existing Item" from the dropdown.
+- Change the filter mode to "All Files" in the new pop-up.
+- Navigate to ULTRAKILL's root.
+
+**FOR SETTING UP THE LIB FOLDER (BEPINEX)**
+- Find "BepInEx.dll" and "0Harmony.dll" in "BepInEx/Core".
+- Select both by holding CTRL and then click Add.
+
+**FOR SETTING UP THE LIB FOLDER (ULTRAKILL)**
+Depending on what your mod is and what it does, some .dll files may not be needed. You will have to experiment with what .dll(s) you need. **However some .DLL(s) are mandatory so do not skip this step.**
+- Navigate to "ULTRAKILL_Data/Managed".
+- Select "Assembly-CSharp.dll" and "UnityEngine.dll" **(MANDATORY)**
+- From there, select any other .DLL(s) that are/is needed for your mod.
+
+**FOR SETTING UP THE REFERENCES**
+- Right click the template and click "Copy Full Path" from the dropdown. (Optional)
+- Right click "References" in the project and select "Add Reference..." from the dropdown.
+- Click on "Browse" on the new pop-up.
+- Navigate to the project's "lib" folder. (If you copied the project path earlier, go there.)
+- Select all ".dll" files inside of the lib folder by hitting CTRL + A.
+- Click Add on the pop-up.
+- Click Ok on the "Reference Manager" window.
+
+# RECOMMENDED PROGRAMS
+- [dnSpyEx](https://github.com/dnSpyEx) is a continuation of dnSpy which allows you to view .NET dlls. Useful for looking at ULTRAKILL's code for modding.
+- [Spite](https://discord.com/invite/envy-spite-1227272001719111750) is a level editor that allows you to look at how ULTRAKILL levels are made and create bundle files for mods. (Rude can also be used as an alternative.)
+

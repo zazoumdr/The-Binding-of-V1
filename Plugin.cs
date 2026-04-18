a11930a59f6aca213f27d3e510080ab1bc7433ff
Bertrand Killian <killianzazou13@gmail.com>
2026-04-19 00:37:01 +0200

    added the first harmony for the soda pop!

--- Plugin.cs
+++ Plugin.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 using HarmonyLib;
 using GameConsole.pcon;
 
-namespace ULTRAKILLBepInExTemplate
+namespace TheBindingOfV1
 {
     /// <summary>
     /// Don't panic if you see errors here! Just follow the instructions in "README.md"
@@ -64,5 +64,70 @@ namespace ULTRAKILLBepInExTemplate
         //         // Code here.
         //     }
         // }
+
+
+        //[HarmonyPatch(typeof(Revolver), "Shoot")]
+        //public static class RevolverFireRatePatch
+        //{
+        //    public static float fireRateMultiplier = 2f;
+
+        //    [HarmonyPostfix]
+        //    public static void Postfix(Revolver __instance)
+        //    {
+        //        if (fireRateMultiplier <= 1f) return;
+
+        //        // Après le tir, shootCharge est à 0 et shootReady à false
+        //        // On le remonte artificiellement selon le multiplicateur
+        //        __instance.shootCharge = 100f * (fireRateMultiplier - 1f);
+        //    }
+        //}
+
+        public class ItemPickup : MonoBehaviour
+        {
+            private void OnTriggerEnter(Collider other)
+            {
+                NewMovement player = other.GetComponent<NewMovement>();
+                if (player == null) return;
+
+                player.walkSpeed += 20f;
+                //RevolverFireRatePatch.fireRateMultiplier = 2f;
+
+                Debug.Log("Soda Quelconque ramassé !");
+                Destroy(gameObject);
+            }
+        }
+
+        // Patch — drop item à la mort d'un Filth
+        [HarmonyPatch(typeof(EnemyIdentifier), "Death", new Type[] { })]
+        public static class FilthDropPatch
+        {
+            [HarmonyPostfix]
+            public static void Postfix(EnemyIdentifier __instance)
+            {
+                if (__instance.enemyType != EnemyType.Filth) return;
+                if (UnityEngine.Random.value > 0.15f) return;
+
+                GameObject item = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                item.transform.position = __instance.transform.position + Vector3.up;
+                item.transform.localScale = Vector3.one * 0.5f;
+                item.GetComponent<Collider>().isTrigger = true;
+                item.AddComponent<ItemPickup>();
+
+                Debug.Log("Item dropped !");
+            }
+        }
+
+
+
+
     }
+    //[HarmonyPatch(typeof(NewMovement), "Start")]
+    //public static class TestMoveSpeedPatch
+    //{
+    //    [HarmonyPostfix]
+    //    public static void Postfix(NewMovement __instance)
+    //    {
+    //        __instance.walkSpeed *= 5f;
+    //    }
+    //}
 }

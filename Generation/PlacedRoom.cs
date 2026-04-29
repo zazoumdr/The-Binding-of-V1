2b7036943a7559c6c3efd980ce17dbdb7fa26f98
Bertrand Killian <killianzazou13@gmail.com>
2026-04-29 22:33:05 +0200

    Added PlacedRoom.cs from issue #18

---
+++ Generation/PlacedRoom.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TheBindingOfV1.Generation
+{
+    public class PlacedRoom
+    {
+        public RoomData room;
+        public Vector3 position;
+        public int floor;
+        public Bounds bounds;
+
+        // Stacking relationships
+        public List<PlacedRoom> stackedAbove = new List<PlacedRoom>();
+        public List<PlacedRoom> stackedBelow = new List<PlacedRoom>();
+
+        // Graph connections (filled by GraphGenerator)
+        public List<PlacedRoom> connectedRooms = new List<PlacedRoom>();
+    }
+}
